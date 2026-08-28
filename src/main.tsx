0548eae6b66430d34e7a05bc254a62ab8e9356e1
Cyber 10X Tech <cyber10xtech@gmail.com>
2026-08-28 16:41:30 +0100

    chore: install Capgo updater foundation

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { CapacitorUpdater } from '@capgo/capacitor-updater';
+import { Capacitor } from '@capacitor/core';
+
+if (Capacitor.isNativePlatform()) {
+  CapacitorUpdater.notifyAppReady();
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
